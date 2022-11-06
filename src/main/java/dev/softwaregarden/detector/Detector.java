111f7200fd6a70b91f2eecaa270548217bead32d
Piotr Przybył <23506256+pioorg@users.noreply.github.com>
2022-11-06 22:13:45 +0100

    Initial commit

---
+++ src/main/java/dev/softwaregarden/detector/Detector.java
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (C) 2022 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package dev.softwaregarden.detector;
+
+import com.sun.net.httpserver.*;
+import static java.time.Duration.*;
+
+import java.io.*;
+import java.math.*;
+import java.net.*;
+import java.nio.charset.*;
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+public class Detector {
+
+    private final BigDecimal alarmLevel = new BigDecimal("0.0004");
+    private final AtomicLong coParticlesDetected = new AtomicLong();
+    private final AtomicLong otherParticlesDetected = new AtomicLong();
+    private final HttpServer server;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("starting=" + Instant.now());
+        int port = Integer.parseInt(System.getenv("detector.port"));
+        String label = Optional.ofNullable(System.getenv("detector.label")).orElseGet(() -> UUID.randomUUID().toString());
+
+        var detector = new Detector(port);
+        detector.startOperating();
+        System.out.println("started=" + Instant.now());
+    }
+
+    public Detector(int port) throws IOException {
+        server = HttpServer.create(new InetSocketAddress(port), 1_000_000);
+        server.createContext("/", exchange -> {
+            var response = "";
+            switch (exchange.getRequestMethod()) {
+                case "POST" -> captureParticle(new String(exchange.getRequestBody().readAllBytes()));
+                case "GET" -> response = prepareReport();
+                case default, null -> {
+                    exchange.sendResponseHeaders(405, 0);
+                    exchange.close();
+                    return;
+                }
+            }
+
+            var responseBytes = response.getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("content-type", "application/json");
+            exchange.sendResponseHeaders(200, responseBytes.length);
+            exchange.getResponseBody().write(responseBytes);
+            exchange.close();
+        });
+        server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    private void startOperating() {
+        var buzzerExecutor = Executors.newScheduledThreadPool(5);
+        buzzerExecutor.scheduleAtFixedRate(this::performCheck, 1, 1, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(ofSeconds(3));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        server.start();
+    }
+
+    private void performCheck() {
+        System.out.println("checking=" + Instant.now());
+        // BAD CODE AHEAD! DO NOT DO YOUR PROD SYSTEM LIKE THIS! DEMO ONLY!
+        var co = BigDecimal.valueOf(coParticlesDetected.get());
+        var all = co.add(BigDecimal.valueOf(otherParticlesDetected.get()));
+        if (co.divide(all, RoundingMode.HALF_EVEN).compareTo(alarmLevel) > 0) {
+            System.out.println("ALARM");
+        }
+    }
+
+    private String prepareReport() {
+        // BAD CODE AHEAD! DO NOT DO YOUR PROD SYSTEM LIKE THIS! DEMO ONLY!
+        return """
+            {
+              "CO" : %d,
+              "other" : %d
+            }""".formatted(coParticlesDetected.get(), otherParticlesDetected.get());
+    }
+
+    private void captureParticle(String particle) {
+        if ("CO".equalsIgnoreCase(particle)) {
+            coParticlesDetected.addAndGet(1);
+        } else {
+            otherParticlesDetected.addAndGet(1);
+        }
+    }
+}
---
+++ src/test/java/dev/softwaregarden/detector/DetectorTest.java
@@ -0,0 +1,139 @@
+/*
+ *  Copyright (C) 2022 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package dev.softwaregarden.detector;
+
+import static dev.softwaregarden.detector.SneakyRunner.*;
+import static java.net.http.HttpRequest.BodyPublishers.*;
+import static java.time.Duration.*;
+import static java.time.Instant.parse;
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.function.*;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.*;
+import org.testcontainers.shaded.org.awaitility.*;
+import org.testcontainers.utility.*;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpResponse.*;
+import java.nio.file.*;
+import java.util.concurrent.*;
+
+class DetectorTest {
+
+    private final MountableFile jar = MountableFile.forHostPath(Paths.get("target/detector-1.0-SNAPSHOT.jar").toAbsolutePath());
+
+    private GenericContainer<?> createContainer() {
+        return new GenericContainer<>("eclipse-temurin:19-alpine")
+            .withCopyFileToContainer(jar, "/tmp/detector.jar")
+            .withExposedPorts(7890)
+            .withEnv("detector.port", "7890")
+            .withCommand("java", "--enable-preview", "-jar", "/tmp/detector.jar");
+    }
+
+    @Test
+//    @Disabled
+    void shouldStartInLessThan4Seconds() {
+
+        try (var container = createContainer()) {
+
+            container.start();
+            System.out.println(container.getLogs());
+
+            var events = container.getLogs().lines()
+                .filter(s -> s.startsWith("start"))
+                .collect(toMap(s -> s.split("=")[0], s -> parse(s.split("=")[1])));
+
+            assertThat(events).containsKeys("started", "started");
+            assertThat(container.getLogs()).contains("checking=");
+            var startedDeadline = events.get("starting").plusSeconds(4);
+            assertThat(events.get("started")).isBeforeOrEqualTo(startedDeadline);
+        }
+    }
+
+    @Test
+    @Disabled
+    void shouldCheckAtLeastOncePerSecond() {
+        try (var container = createContainer()) {
+
+            container.start();
+
+            var checkEvents = container.getLogs().lines()
+                .filter(s -> s.startsWith("checking="))
+                .toList();
+
+            Awaitility.given()
+                .between(ofSeconds(3), ofSeconds(4))
+                .pollDelay(ofSeconds(3))
+                .until(
+                    () -> container.getLogs().lines().filter(s -> s.startsWith("checking=")).toList(),
+                    before -> before.size() - checkEvents.size() >= 3
+                );
+        }
+    }
+
+
+    @Test
+    @Disabled
+    void shouldAlarmAbove400PPM() throws InterruptedException {
+
+        try (var container = createContainer()) {
+
+            container.start();
+
+            var client = HttpClient.newHttpClient();
+            var uri = URI.create("http://%s:%d/".formatted(container.getHost(), container.getFirstMappedPort()));
+            var o2Req = HttpRequest.newBuilder(uri).POST(ofString("O2")).build();
+            var coReq = HttpRequest.newBuilder(uri).POST(ofString("CO")).build();
+
+            try (var exec = Executors.newVirtualThreadPerTaskExecutor()) {
+                for (int i = 0; i < 1_000; i++) {
+                    exec.execute(throwingRun(() -> client.send(o2Req, BodyHandlers.discarding())));
+                }
+
+                exec.execute(throwingRun(() -> client.send(coReq, BodyHandlers.discarding())));
+
+                exec.shutdown();
+                assertThat(exec.awaitTermination(10, TimeUnit.SECONDS)).isTrue();
+            }
+
+            Awaitility.given()
+                .atMost(ofSeconds(1))
+                .until(
+                    container::getLogs,
+                    logs -> logs.contains("ALARM")
+                );
+//            var checkRequest = HttpRequest.newBuilder(uri).GET().build();
+//            var response = client.send(checkRequest, BodyHandlers.ofString());
+//            System.out.println(response.body());
+        }
+    }
+
+}
+
+interface SneakyRunner {
+    static Runnable throwingRun(ThrowingRunnable tr) {
+        return () -> {
+            try {
+                tr.run();
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
